0ab2aed0835f1079eeaa17d1a274609000a6deb4
naeem676 <ali.naeem939@gmail.com>
2020-07-27 11:50:15 +0600

    value is show on input

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ pinBtn.addEventListener("click", function(){
           
           
           const newPin =  Math.floor(Math.random() * (2000 - 1001)) +  1001;
-          document.getElementById("pinInput").value = newPin;
-          
-
-
-          
+          document.getElementById("pinInput").value = newPin;       
           
 })
 document.getElementById("pinInput").value = "";
+
+let submitInput = document.getElementById("submitInput");
+function calculate(data) {
+          switch (data) {
+                    case 'c':
+                              submitInput.value = "";
+                              break;
+                    case '<':
+                              submitInput.value = submitInput.value.substr(0, submitInput.value.length - 1);
+                              break;
+          
+                    default:
+                              submitInput.value += data;
+                              break;
+          }
+
+          
+}
+document.getElementById("submitInput").value = "";
